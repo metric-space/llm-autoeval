3bbaf49b0353e7c76f5c8b23077b6801350f32aa
Mark McQuade <mark@newagetechie.com>
2024-03-02 11:31:27 -0500

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -82,6 +82,52 @@ if [ "$BENCHMARK" == "nous" ]; then
     
     python ../llm-autoeval/main.py . $(($end-$start))
 
+if [ "$BENCHMARK" == "nous-peft" ]; then
+    git clone -b add-agieval https://github.com/dmahan93/lm-evaluation-harness
+    cd lm-evaluation-harness
+    pip install -e .
+
+    benchmark="agieval"
+    python main.py \
+        --model hf-causal-experimental \
+        --model_args pretrained=$MODEL_ID,trust_remote_code=$TRUST_REMOTE_CODE \
+        --tasks agieval_aqua_rat,agieval_logiqa_en,agieval_lsat_ar,agieval_lsat_lr,agieval_lsat_rc,agieval_sat_en,agieval_sat_en_without_passage,agieval_sat_math \
+        --device cuda:$cuda_devices \
+        --batch_size auto \
+        --output_path ./${benchmark}.json
+
+    benchmark="gpt4all"
+    python main.py \
+        --model hf-causal-experimental \
+        --model_args pretrained=$MODEL_ID,trust_remote_code=$TRUST_REMOTE_CODE \
+        --tasks hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa \
+        --device cuda:$cuda_devices \
+        --batch_size auto \
+        --output_path ./${benchmark}.json
+
+    benchmark="truthfulqa"
+    python main.py \
+        --model hf-causal-experimental \
+        --model_args pretrained=$MODEL_ID,trust_remote_code=$TRUST_REMOTE_CODE \
+        --tasks truthfulqa_mc \
+        --device cuda:$cuda_devices \
+        --batch_size auto \
+        --output_path ./${benchmark}.json
+
+    benchmark="bigbench"
+    python main.py \
+        --model hf-causal-experimental \
+        --model_args pretrained=$MODEL_ID,trust_remote_code=$TRUST_REMOTE_CODE \
+        --tasks bigbench_causal_judgement,bigbench_date_understanding,bigbench_disambiguation_qa,bigbench_geometric_shapes,bigbench_logical_deduction_five_objects,bigbench_logical_deduction_seven_objects,bigbench_logical_deduction_three_objects,bigbench_movie_recommendation,bigbench_navigate,bigbench_reasoning_about_colored_objects,bigbench_ruin_names,bigbench_salient_translation_error_detection,bigbench_snarks,bigbench_sports_understanding,bigbench_temporal_sequences,bigbench_tracking_shuffled_objects_five_objects,bigbench_tracking_shuffled_objects_seven_objects,bigbench_tracking_shuffled_objects_three_objects \
+        --device cuda:$cuda_devices \
+        --batch_size auto \
+        --output_path ./${benchmark}.json
+
+    end=$(date +%s)
+    echo "Elapsed Time: $(($end-$start)) seconds"
+    
+    python ../llm-autoeval/main.py . $(($end-$start))    
+
 elif [ "$BENCHMARK" == "openllm" ]; then
     git clone https://github.com/EleutherAI/lm-evaluation-harness
     cd lm-evaluation-harness
