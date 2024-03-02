352ed26e29289317f5fd5132969b3bca0aba7666
Mark McQuade <mark@newagetechie.com>
2024-03-02 12:13:19 -0500

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -82,7 +82,7 @@ if [ "$BENCHMARK" == "nous" ]; then
     
     python ../llm-autoeval/main.py . $(($end-$start))
 
-if [ "$BENCHMARK" == "peft-nous" ]; then
+elif [ "$BENCHMARK" == "peft-nous" ]; then
     git clone -b add-agieval https://github.com/dmahan93/lm-evaluation-harness
     cd lm-evaluation-harness
     pip install -e .
