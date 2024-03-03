87f2cd0cae9ce2ac8d83f2743f07019af74ba985
Mark McQuade <mark@newagetechie.com>
2024-03-02 20:51:41 -0500

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -93,7 +93,7 @@ elif [ "$BENCHMARK" == "peft-nous" ]; then
         --model_args pretrained=$MODEL_ID,trust_remote_code=$TRUST_REMOTE_CODE \
         --tasks agieval_aqua_rat,agieval_logiqa_en,agieval_lsat_ar,agieval_lsat_lr,agieval_lsat_rc,agieval_sat_en,agieval_sat_en_without_passage,agieval_sat_math \
         --device cuda:0 \
-        --batch_size auto \
+        # --batch_size auto \
         --output_path ./${benchmark}.json
 
     benchmark="gpt4all"
@@ -102,7 +102,7 @@ elif [ "$BENCHMARK" == "peft-nous" ]; then
         --model_args pretrained=$MODEL_ID,trust_remote_code=$TRUST_REMOTE_CODE \
         --tasks hellaswag,openbookqa,winogrande,arc_easy,arc_challenge,boolq,piqa \
         --device cuda:0 \
-        --batch_size auto \
+        # --batch_size auto \
         --output_path ./${benchmark}.json
 
     benchmark="truthfulqa"
@@ -111,7 +111,7 @@ elif [ "$BENCHMARK" == "peft-nous" ]; then
         --model_args pretrained=$MODEL_ID,trust_remote_code=$TRUST_REMOTE_CODE \
         --tasks truthfulqa_mc \
         --device cuda:0 \
-        --batch_size auto \
+        # --batch_size auto \
         --output_path ./${benchmark}.json
 
     benchmark="bigbench"
@@ -120,7 +120,7 @@ elif [ "$BENCHMARK" == "peft-nous" ]; then
         --model_args pretrained=$MODEL_ID,trust_remote_code=$TRUST_REMOTE_CODE \
         --tasks bigbench_causal_judgement,bigbench_date_understanding,bigbench_disambiguation_qa,bigbench_geometric_shapes,bigbench_logical_deduction_five_objects,bigbench_logical_deduction_seven_objects,bigbench_logical_deduction_three_objects,bigbench_movie_recommendation,bigbench_navigate,bigbench_reasoning_about_colored_objects,bigbench_ruin_names,bigbench_salient_translation_error_detection,bigbench_snarks,bigbench_sports_understanding,bigbench_temporal_sequences,bigbench_tracking_shuffled_objects_five_objects,bigbench_tracking_shuffled_objects_seven_objects,bigbench_tracking_shuffled_objects_three_objects \
         --device cuda:0 \
-        --batch_size auto \
+        # --batch_size auto \
         --output_path ./${benchmark}.json
 
     end=$(date +%s)
